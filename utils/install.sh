beba7cbd927551a6e3ac8edddf45b032080be39a
gmnr <guido.minieri@gmail.com>
2019-07-16 23:09:27 +0200

    fixed display bug in installation script

--- utils/install.sh
+++ utils/install.sh
@@ -6,33 +6,33 @@
 dir=~/.dotfiles
 
 # change in the working directory of the files to be symlinked in the home 
-echo "Changing to the $dir directory"
 cd $dir
-files=`find . -maxdepth 1 -type f -printf "%f\n" | grep -v .gitmodules`    # all except .gitmodules
-echo "Fetched list of files to be symlinked"
+echo "Fetching the list of files to be symlinked in the home directory"
+files=`find . -maxdepth 1 -type f -exec basename {} \; | grep -v .gitmodules`    # all except .gitmodules
 echo
 
-
 # handle alacritty installation with special config
-ln -s $dir/config/alacritty.yml ~/.config/alacritty/alacritty.yml
+echo "Creating symlink for alacritty"
+ln -s $dir/config/alacritty.yml ~/.config/alacritty/alacritty.yml 2> /dev/null
+
+# handle vim special location
+echo "Creating symlink for .vimrc"
+ln -s $dir/.vim/.vimrc ~/.vimrc 2> /dev/null
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-        echo "Creating symlink to $file in home directory."
-        ln -s $dir/$file ~/$file 2> /dev/null
+    echo "Creating symlink for $file"
+    ln -s $dir/$file ~/$file 2> /dev/null
 done
-
 
 # if on mac prompt for loading preferred config
 if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo
     echo "Do you want also to load the preferred config for mac?"
     read ans
     if [[ $ans == "yes" ]]; then
         ./$dir/macos/.macos
     else
-        echo "ok, next time"
+        echo "Ok... exiting"
     fi
 fi
-
-
-
