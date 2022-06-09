609acded4281c7ea7f83de5ec7a0a7dde8e4dcdf
gmnr <guido.minieri@gmail.com>
2022-06-09 09:37:26 +0200

    Update tasks script

--- .task_script/tasks.sh
+++ .task_script/tasks.sh
@@ -5,13 +5,13 @@ if [[ $1 -eq 1 ]]
 fi
 if [[ $1 -eq 2 ]]
   # get all the tasks that have not been delegated nor blocked/waiting
-  then echo $(/usr/local/bin/task folder:next count)" "
+  then echo $(/usr/local/bin/task folder:next -WAITING count)" "
 fi
 if [[ $1 -eq 3 ]]
-  # get all the tasks that are not in inbox and that are waiting (for example the activities delegated)
-  then echo $(/usr/local/bin/task folder:inbox +WAITING count)" "
+  # get all the tasks that are not in inbox and that are waiting
+  then echo $(/usr/local/bin/task +WAITING folder.not:inbox count)" "
 fi
 if [[ $1 -eq 4 ]]
-  # get all the tasks that are scheduled for later (es. generic ideas/maybes) subject to weekly review
-  then echo $(/usr/local/bin/task folder:later count)" "
+  # get all the tasks that are scheduled for later
+  then echo $(/usr/local/bin/task +SCHEDULED folder.not:inbox count)" "
 fi
