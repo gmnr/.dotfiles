7cfdd1c046f2518d8a530f2e21c6a7d8b952e82f
gmnr <guido.minieri@gmail.com>
2022-08-03 18:49:27 +0200

    Update dir structure

--- .utils/install.sh
+++ .utils/install.sh
@@ -3,12 +3,12 @@
 # change in the working directory of the files to be symlinked in the home 
 cd $DOTFILES
 echo "Fetching the list of files to be symlinked in the home directory"
-files=`find ./.config -maxdepth 1 -type f -exec basename {} \; | grep -v .gitmodules` | grep -v .macos # all except .gitmodules
+files=`find $DOTFILES/config -maxdepth 1 -type f -exec basename {} \; | grep -v .macos`  # all except .gitmodules
 echo
 
 # handle alacritty installation with special config
 echo "Creating symlink for alacritty"
-ln -s $DOTFILES/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml 2> /dev/null
+ln -s $DOTFILES/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml 2> /dev/null
 
 # handle vim special location
 echo "Creating symlink for vim configuration"
@@ -17,7 +17,7 @@ ln -s $DOTFILES/nvim/ ~/.config/ 2> /dev/null
 # create the symlinks for the files in the .dotfiles folder
 for file in $files; do
     echo "Creating symlink for $file"
-    ln -s $DOTFILES/.config/$file ~/$file 2> /dev/null
+    ln -s $DOTFILES/config/$file ~/$file 2> /dev/null
 done
 
 # if on mac prompt for loading preferred config
@@ -26,7 +26,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     echo "Do you want also to load the preferred config for mac?"
     read ans
     if [[ $ans == "yes" ]]; then
-        ./$DOTFILES/.config/.macos
+        ./$DOTFILES/config/.macos
     else
         echo "Ok... exiting"
     fi
