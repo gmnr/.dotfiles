b91ff229c0c15706e33a5bc4670c2d69840a4c05
gmnr <guido.minieri@gmail.com>
2022-08-24 20:41:43 +0200

    Added more options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,48 +1,56 @@
+local opt = vim.opt
 -- basic options
-vim.opt.number        = true  -- enable numbering
-vim.opt.termguicolors = true  -- enable termguicolors
-vim.opt.shiftround    = true  -- round indent to multiples of shiftwidth
-vim.opt.cursorline    = true  -- enable cursorline
-vim.opt.ignorecase    = true  -- ignore 
-vim.opt.smartcase     = true  -- override ignorecase if capital in search pattern
-vim.opt.expandtab     = true  -- use spaces while tabbing
-vim.opt.lazyredraw    = true  -- don't redraw screen while executing macros
-vim.opt.gdefault      = true  -- when substituting the g flag is always enabled
-vim.opt.smartindent   = true  -- smart autoindenting
-vim.opt.splitright    = true  -- open next window to the right of current one
-vim.opt.splitbelow    = true  -- open next window below the current one
-vim.opt.showmode      = false -- hide mode on last row
-vim.opt.hlsearch      = false -- disable highlight of search results
-vim.opt.swapfile      = false -- disable swapfile
-vim.opt.signcolumn    = 'yes' -- always enable signcolumn
-vim.opt.tabstop       = 2     -- number of spaces a tab counts for
-vim.opt.shiftwidth    = 2     -- number of spaces of each indentation level
-vim.opt.softtabstop   = 2     -- insert spaces when tabbing
-vim.opt.scrolloff     = 999   -- keep cursor in the middle of the line
+opt.number                     = true  -- enable numbering
+opt.termguicolors              = true  -- enable termguicolors
+opt.shiftround                 = true  -- round indent to multiples of shiftwidth
+opt.cursorline                 = true  -- enable cursorline
+opt.ignorecase                 = true  -- ignore 
+opt.smartcase                  = true  -- override ignorecase if capital in search pattern
+opt.expandtab                  = true  -- use spaces while tabbing
+opt.lazyredraw                 = true  -- don't redraw screen while executing macros
+opt.gdefault                   = true  -- when substituting the g flag is always enabled
+opt.smartindent                = true  -- smart autoindenting
+opt.splitright                 = true  -- open next window to the right of current one
+opt.splitbelow                 = true  -- open next window below the current one
+opt.showmode                   = false -- hide mode on last row
+opt.hlsearch                   = false -- disable highlight of search results
+opt.swapfile                   = false -- disable swapfile
+opt.signcolumn                 = 'yes' -- always enable signcolumn
+opt.tabstop                    = 2     -- number of spaces a tab counts for
+opt.shiftwidth                 = 2     -- number of spaces of each indentation level
+opt.softtabstop                = 2     -- insert spaces when tabbing
+opt.scrolloff                  = 999   -- keep cursor in the middle of the line
+opt.wildignorecase             = true  -- ignore case when writing commands
+
+-- diff options
+opt.diffopt                    = 'filler,iwhite,internal,algorithm:patience'
+
+-- preview opt
+opt.completeopt                = 'menu,menuone,noselect'
 
 -- spelling
-vim.opt.spelllang     = 'en_us,it'  -- set language of spell
+opt.spelllang                  = 'en_us,it'  -- set language of spell
 
 -- grep settings (use ripgrep)
-vim.opt.grepprg       = 'rg --vimgrep --no-heading --smart-case'
-vim.opt.grepformat    = '%f:%l:%c:%m,%f:%l:%m'
+opt.grepprg                    = 'rg --vimgrep --no-heading --smart-case'
+opt.grepformat                 = '%f:%l:%c:%m,%f:%l:%m'
 
 -- disable some useless standard plugins to save startup time these features are better covered by plugins
-vim.g.loaded_gzip = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
+vim.g.loaded_gzip              = 1
+vim.g.loaded_tar               = 1
+vim.g.loaded_tarPlugin         = 1
+vim.g.loaded_zip               = 1
+vim.g.loaded_zipPlugin         = 1
+vim.g.loaded_getscript         = 1
+vim.g.loaded_getscriptPlugin   = 1
+vim.g.loaded_vimball           = 1
+vim.g.loaded_vimballPlugin     = 1
+vim.g.loaded_matchit           = 1
+vim.g.loaded_matchparen        = 1
+vim.g.loaded_2html_plugin      = 1
+vim.g.loaded_logiPat           = 1
+vim.g.loaded_rrhelper          = 1
+vim.g.loaded_netrw             = 1
+vim.g.loaded_netrwPlugin       = 1
+vim.g.loaded_netrwSettings     = 1
 vim.g.loaded_netrwFileHandlers = 1
