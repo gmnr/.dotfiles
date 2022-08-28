791ead6896e979de15db7dbcd996c6bd38144e22
gmnr <guido.minieri@gmail.com>
2022-08-28 21:54:59 +0200

    Add autocmd to indent ledger files correctly

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,19 +1,27 @@
--- Autocommands
+-- Import templates for python files
 vim.api.nvim_create_autocmd('BufNewFile', {
   pattern = {'*.py'},
   desc = 'Use template for python files',
   command = '0r ~/.dotfiles/nvim/templates/python.template'
 })
 
--- FileType autocommands
+-- Enable spellign for verbose text editing
 vim.api.nvim_create_autocmd('FileType', {
   pattern = {'markdown', 'vimwiki'},
   desc = 'Enable spelling while editing verbose files',
   command = 'setlocal spell'
 })
 
+-- Set a fixed textwidth for verbose files
 vim.api.nvim_create_autocmd('FileType', {
   pattern = {'markdown', 'vimwiki'},
   desc = 'Verbose texts should have a default textwidth',
   command = 'setlocal tw=80'
+})
+
+-- 4 spaces for indent for ledger files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'ledger'},
+  desc = 'Indent at 4 spaces',
+  command = 'setlocal ts=4 sw=4 sts=4'
 })
