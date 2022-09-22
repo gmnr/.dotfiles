5fe6b479f3482d6e6a96fa81ca6a4df9335eeb6f
gmnr <guido.minieri@gmail.com>
2022-09-22 18:46:46 +0200

    move `signcolumn` opt to the correct config file

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -23,6 +23,7 @@ opt.softtabstop                = 2     -- insert spaces when tabbing
 opt.scrolloff                  = 999   -- keep cursor in the middle of the line
 opt.wildignorecase             = true  -- ignore case when writing commands
 opt.complete                   = ''    -- disable ordinary autocompletion
+opt.signcolumn                 = 'yes' -- always enable signcolumn
 
 -- diff options
 opt.diffopt                    = 'filler,iwhite,internal,algorithm:patience'
