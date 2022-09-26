a6a3d1a9330fac6dd214dfd22bc8ef669b694d1d
gmnr <guido.minieri@gmail.com>
2022-09-26 23:09:04 +0200

    config capture custom templates

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -4,10 +4,24 @@ function M.config()
   require('orgmode').setup_ts_grammar()
   require('orgmode').setup({
     org_agenda_files = {'~/.notes/*'},
-    org_default_notes_file = '~/.notes/refile.org',
+    org_default_notes_file = '~/.notes/inbox.org',
     org_hide_leading_stars = true,
     mappings = {
-      prefix = '<C-r>'
+      prefix = '<C-r>',
+      global = {
+        org_agenda = '<C-r>p',
+        org_capture = '<C-r>l'
+      }
+    },
+    org_capture_templates = {
+      T = { description = 'Tickler',
+      template = '* %? \n %u',
+      target = '~/.notes/tickler.org'
+      },
+      t = { description = 'Task',
+      template = '* TODO %?',
+      target = '~/.notes/inbox.org'
+      }
     }
   })
 end
