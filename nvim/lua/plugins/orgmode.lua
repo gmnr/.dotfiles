b7ffeb739914605d3026e548d553fa627109cfe5
gmnr <guido.minieri@gmail.com>
2022-09-27 15:51:11 +0200

    add new todo states (and colors) / indent

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -5,22 +5,29 @@ function M.config()
   require('orgmode').setup({
     org_agenda_files = {'~/.notes/*'},
     org_default_notes_file = '~/.notes/inbox.org',
+    org_todo_keywords = {'TODO', 'WAITING', '|', 'DONE', 'CANCELED'},
+    org_todo_keyword_faces = {
+      CANCELED = ':foreground magenta',
+      WAITING = ':foreground orange  :underline on',
+    },
     org_hide_leading_stars = true,
     mappings = {
       prefix = '<C-r>',
       global = {
-        org_agenda = '<C-r>p',
+        org_agenda = '<C-r>h',
         org_capture = '<C-r>l'
       }
     },
     org_capture_templates = {
-      T = { description = 'Tickler',
-      template = '* %? \n %u',
-      target = '~/.notes/tickler.org'
+      T = {
+        description = 'Tickler',
+        template = '* %? \n %u',
+        target = '~/.notes/tickler.org'
       },
-      t = { description = 'Task',
-      template = '* TODO %?',
-      target = '~/.notes/inbox.org'
+      t = {
+        description = 'Task',
+        template = '* TODO %?',
+        target = '~/.notes/inbox.org'
       }
     }
   })
