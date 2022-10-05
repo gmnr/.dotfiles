ef6507b1e93cefe9013ab787d97557a3c779dcd4
gmnr <guido.minieri@gmail.com>
2022-10-05 17:19:56 +0200

    further simplify org config

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -4,7 +4,6 @@ function M.config()
   require('orgmode').setup_ts_grammar()
   require('orgmode').setup({
     org_agenda_files = {'~/.notes/*'},
-    org_default_notes_file = '~/.notes/inbox.org',
     org_todo_keywords = {'TODO', 'NEXT', 'HOLD', '|', 'DONE', 'CANCELED'},
     org_todo_keyword_faces = {
       NEXT = ':foreground cyan',
