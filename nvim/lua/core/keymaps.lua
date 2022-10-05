13e5b42e159e11b40f3dbf41dfa1962d25ba0b62
gmnr <guido.minieri@gmail.com>
2022-10-05 22:16:19 +0200

    put brackets in the correct format

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -10,13 +10,13 @@ vim.keymap.set('n', 'j', 'gj')
 vim.keymap.set('n', 'k', 'gk')
 
 -- open and load .vimrc"
-vim.keymap.set('n', '<leader>ev', ':lua require("telescope.builtin").find_files({cwd="~/.dotfiles/nvim/lua/", search_dirs={"plugins", "core"}})<CR>', {silent=true})
+vim.keymap.set('n', '<leader>ev', ':lua require("telescope.builtin").find_files({cwd="~/.dotfiles/nvim/lua/", search_dirs={"plugins", "core"}})<CR>', { silent = true })
 vim.keymap.set('n', '<leader>eu', ':PlugUpdate<CR>')
 vim.keymap.set('n', '<leader>ew', ':exe "e ~/.notes/scratch/".strftime("%F").".md" <CR>', { silent = true })
 
 
 -- open notes
-vim.keymap.set('n', '<leader>pl', ':lua require("telescope.builtin").find_files({cwd="~/.notes"})<CR>', {silent=true})
+vim.keymap.set('n', '<leader>pl', ':lua require("telescope.builtin").find_files({cwd="~/.notes"})<CR>', { silent = true })
 
 -- quick save and exit
 vim.keymap.set('n', '<leader>ww', ':w<CR>')
@@ -43,11 +43,11 @@ vim.keymap.set('n', '<leader>gl', ':diffget //3<CR>')
 vim.keymap.set('n', '<leader>gc', ':GBranches<CR>')
 
 -- telescope mappings
-vim.keymap.set('n', '<leader>pp', ':lua Project_files()<CR>', {silent=true})  -- link custom function to use find_git when .git exists and find_files when it doesn't
-vim.keymap.set('n', '<leader>pi', ':Telescope live_grep<CR>', {silent=true})
-vim.keymap.set('n', '<leader>po', ':Telescope buffers<CR>', {silent=true})
-vim.keymap.set('n', '<leader>ps', ':Telescope git_status<CR>', {silent=true})
-vim.keymap.set('n', '<leader>pc', ':Telescope git_branches<CR>', {silent=true})
+vim.keymap.set('n', '<leader>pp', ':lua Project_files()<CR>', { silent = true })  -- link custom function to use find_git when .git exists and find_files when it doesn't
+vim.keymap.set('n', '<leader>pi', ':Telescope live_grep<CR>', { silent = true })
+vim.keymap.set('n', '<leader>po', ':Telescope buffers<CR>', { silent = true })
+vim.keymap.set('n', '<leader>ps', ':Telescope git_status<CR>', { silent = true })
+vim.keymap.set('n', '<leader>pc', ':Telescope git_branches<CR>', { silent = true })
 
 -- change the current directory to edited file
 vim.keymap.set('n', '<leader>cd', ':cd %:h<CR>')
@@ -87,15 +87,15 @@ vim.keymap.set('n', '<leader>hS', ':Gitsigns stage_buffer<CR>')
 vim.keymap.set('n', '<leader>hR', ':Gitsigns reset_buffer<CR>')
 vim.keymap.set('n', '<leader>hd', ':Gitsigns diffthis<CR>')
 vim.keymap.set('n', '<leader>hu', ':Gitsigns undo_stage_hunk<CR>')
-vim.keymap.set({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', {silent=true})
-vim.keymap.set({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', {silent=true})
+vim.keymap.set({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', { silent = true })
+vim.keymap.set({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', { silent = true })
 vim.keymap.set('n', ']c', "&diff ? ']c' : ':Gitsigns next_hunk<CR>'", {expr=true})
 vim.keymap.set('n', '[c', "&diff ? '[c' : ':Gitsigns prev_hunk<CR>'", {expr=true})
 
 -- lsp config
-vim.keymap.set('n', 'K', ':Lspsaga hover_doc<CR>', {silent=true})
-vim.keymap.set('n', '[d', ':Lspsaga diagnostic_jump_prev<CR>', {silent=true})
-vim.keymap.set('n', ']d', ':Lspsaga diagnostic_jump_next<CR>', {silent=true})
+vim.keymap.set('n', 'K', ':Lspsaga hover_doc<CR>', { silent = true })
+vim.keymap.set('n', '[d', ':Lspsaga diagnostic_jump_prev<CR>', { silent = true })
+vim.keymap.set('n', ']d', ':Lspsaga diagnostic_jump_next<CR>', { silent = true })
 vim.keymap.set('n', 'gI', vim.lsp.buf.implementation)
 vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help)
 vim.keymap.set('n', '<leader>ls', vim.diagnostic.setloclist)
@@ -104,11 +104,11 @@ vim.keymap.set('n', '<leader>lm', vim.lsp.buf.formatting)
 vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
 vim.keymap.set('n', 'gr', vim.lsp.buf.references)
-vim.keymap.set('n', '<leader>ll', ':Lspsaga show_line_diagnostics<CR>', {silent=true})
-vim.keymap.set('n', '<leader>lL', ':Lspsaga show_cursor_diagnostics<CR>', {silent=true})
-vim.keymap.set('n', '<leader>la', ':Lspsaga code_action<CR>', {silent=true})
-vim.keymap.set('n', '<leader>lf', ':Lspsaga lsp_finder<CR>', {silent=true})
-vim.keymap.set('n', '<leader>lr', ':Lspsaga rename<CR>', {silent=true})
+vim.keymap.set('n', '<leader>ll', ':Lspsaga show_line_diagnostics<CR>', { silent = true })
+vim.keymap.set('n', '<leader>lL', ':Lspsaga show_cursor_diagnostics<CR>', { silent = true })
+vim.keymap.set('n', '<leader>la', ':Lspsaga code_action<CR>', { silent = true })
+vim.keymap.set('n', '<leader>lf', ':Lspsaga lsp_finder<CR>', { silent = true })
+vim.keymap.set('n', '<leader>lr', ':Lspsaga rename<CR>', { silent = true })
 
 -- open links and files from vim
 vim.keymap.set('n', 'gx', ':!open <C-r><C-a><CR>')
