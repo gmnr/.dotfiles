56d650fad11a82ca054b8c46e73f33790205618c
gmnr <guido.minieri@gmail.com>
2023-01-11 16:30:16 +0100

    restructure dashboard around new prj system

--- tasks/dashboard_tasks.sh
+++ tasks/dashboard_tasks.sh
@@ -40,8 +40,23 @@ if [[ $1 -eq 4 ]]; then
   fi
 fi
 
+# display count for `waiting` tasks
 if [[ $1 -eq 5 ]]; then
   cmd=$(/usr/local/bin/task count "+wait +PENDING")
+  if [[ $cmd -gt 0 ]]; then
+    echo "$cmd "
+  else
+    echo "- "
+  fi
+fi
+
+# display count of unassigned projects
+if [[ $1 -eq 6 ]]; then
+  active=$(/usr/local/bin/task projects +SCHEDULED or +wait or +next | awk 'NR>4 {  print $1 }' | grep ^[a-z]* -w)
+  all=$(/usr/local/bin/task projects | awk 'NR>4 {  print $1 }' | grep ^[a-z]* -w)
+
+  cmd=$(echo -e "$active\n$all" | sort | uniq -c | grep -c "^.*1 ")
+
   if [[ $cmd -gt 0 ]]; then
     echo "$cmd "
   else
