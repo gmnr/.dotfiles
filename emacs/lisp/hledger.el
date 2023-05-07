0363961d981a78fd5009f5d8fc268e9295421587
gmnr <guido.minieri@gmail.com>
2023-05-07 20:14:24 +0200

    custom hledger-mode v0.01

--- emacs/lisp/hledger.el
+++ emacs/lisp/hledger.el
@@ -6,16 +6,13 @@
 (defvar hledger-date-regex "[0-9]\\{4\\}[-/][0-9]\\{2\\}[-/][0-9]\\{2\\}"
   "Regular expression for dates for font lock.")
 
-(defvar hledger-amount-regex "[0-9]*\.?[0-9]+\\(,[0-9][0-9]\\)?\\( [A-Z]+\\)?"
+(defvar hledger-amount-regex "-?[0-9]*\.?[0-9]+\\(,[0-9][0-9]\\)?\\( [A-Z]+\\)?"
   "Regular expression for amounts for font lock.")
 
 (defvar hledger-neg-amount-regex "-[0-9]*\.?[0-9]+\\(,[0-9][0-9]\\)?\\( [A-Z]+\\)?"
   "Regular expression for negative amounts for font lock.")
 
 (defvar hledger-payee-regex "-[0-9][0-9]\\s-\\(.*\\)\\s-|"
-  "Regular expression for payees in font lock.")
-
-(defvar hledger-tag-regex "^  ; \\(.+\\):\\(.+\\)"
   "Regular expression for payees in font lock.")
 
 (defvar hledger-account-regex
@@ -74,8 +71,8 @@
   "Called when the line to be indented is an empty one."
   (cond
    ((hledger-prev-line-emptyp) (hledger-insert-date) (hledger-select-payee))
-   ((hledger-prev-line-has-payeep) (indent-line-to tab-width) (hledger-select-account))
-   ((hledger-prev-line-has-amtp) (indent-line-to tab-width) (hledger-select-account))
+   ((hledger-prev-line-has-payeep) (indent-line-to tab-width) (hledger-select-account) (hledger-get-amount))
+   ((hledger-prev-line-has-amtp) (indent-line-to tab-width) (hledger-select-account) (newline-and-indent))
    (t (indent-line-to tab-width))))
 
 
@@ -98,25 +95,68 @@
   (split-string (shell-command-to-string "hledger -f ~/.finance/all.journal accounts")))
 
 (defun hledger-select-payee ()
+  "Fetch from cache the list of payees for fuzzy insertion"
   (interactive)
   (ivy-read "Select Payee: "
 			hledger-payee-cache
 			:action (lambda (x) (insert x " | "))))
 
 (defun hledger-select-account ()
+  "Fetch from cache the list of accounts for fuzzy insertion"
   (interactive)
   (ivy-read "Select Payee: "
 			hledger-accounts-cache
 			:action (lambda (x) (insert x "  "))))
 
-;; (defun hledger-get-format-amount ()
-;;   (let amt (read-string "Insert amount >> "))
-;;   (thing-at-point 'line t))
+(defun hledger-get-amount ()
+  "Get user input and perform simple calculations"
+  (insert (replace-regexp-in-string "\\." "," (calc-eval (replace-regexp-in-string "," "\." (read-string "Insert Amount ->> ")))))
+  (hledger-format-transaction)
+  (newline-and-indent))
+
+(defun hledger-format-transaction ()
+  "Format transaction to correctly display amount and format"
+  (interactive)
+  (setq val (split-string (buffer-substring-no-properties (line-beginning-position) (line-end-position)) "\s+" t))
+  (delete-region (line-beginning-position) (line-end-position))
+  (setq acc (pop val))
+  (setq amt (hledger-format-amount (pop val)))
+  (setq l-acc (length acc))
+  (setq l-amt (length amt))
+  (setq spaces (- 60 l-amt l-acc))
+  (insert (concat "  " acc))
+  (dotimes (_ spaces) (insert " "))
+  (insert amt))
+
+(defun hledger-format-amount (amt)
+  "Format the amount of the transaction to the following specification '1.000,00'"
+  (setq sides (split-string amt ","))
+  (setq int (pop sides))
+  (setq dec (pop sides))
+  (if (> (length int) 3)
+      (setq int (hledger-thousands int))
+    int)
+  (if (eq dec nil)
+      (setq dec "00")
+    (if (eq (length dec) 1)
+        (setq dec (concat dec "0"))
+      dec))
+  (concat int "," dec))
+
+(defun hledger-thousands (s &optional separator)
+  "Add commas to NUMBER and return it as a string. Optional SEPARATOR is the string to use to separate groups. It defaults to a dot."
+  (let ((num s)
+        (op (or separator ".")))
+    (while (string-match "\\(.*[0-9]\\)\\([0-9][0-9][0-9].*\\)" num)
+      (setq num (concat 
+                 (match-string 1 num) op
+                 (match-string 2 num))))
+    num))
 
 ;; MODE SETUP
 
 (defcustom hledger-mode-hook nil
-  "Normal hook for entering ‘hledger-mode’."
+  "Normal hook for entering 'hledger-mode'."
   :type 'hook
   :group 'hledger)
 
@@ -157,8 +197,6 @@
    `(,hledger-payee-regex 1 hledger-payee-face)
    `(,hledger-sep-regex . hledger-sep-face)
    `(,hledger-date-regex . hledger-date-face)
-   `(,hledger-tag-regex 1 hledger-sep-face)
-   `(,hledger-tag-regex 2 hledger-date-face)
    `(,hledger-amount-regex . hledger-amount-face)))
 
 (defun hledger-font-lock-defaults ()
@@ -166,10 +204,10 @@
   (list (hledger-font-lock-keywords-1)))
 
 (defvar hledger-mode-syntax-table (let ((st (make-syntax-table)))
-									(modify-syntax-entry ?: "_" st)
-									(modify-syntax-entry ?\; "<" st)
-									(modify-syntax-entry ?\n ">" st)
-									st)
+                                    (modify-syntax-entry ?: "_" st)
+                                    (modify-syntax-entry ?\; "<" st)
+                                    (modify-syntax-entry ?\n ">" st)
+                                    st)
   "Syntax table for hledger mode.")
 
 (defun hledger-mode-init ()
@@ -179,6 +217,7 @@
   (setq-local comment-start "; ")
   (setq-local comment-end "")
   (setq-local require-final-newline t)
+  (setq-local tab-width 2)
   (setq hledger-accounts-cache (hledger-get-accounts))
   (setq hledger-payee-cache (hledger-get-payees)))
 
@@ -193,5 +232,3 @@
 (add-to-list 'auto-mode-alist '("\\.journal\\'" . hledger-mode))
 
 
-;; set tab-width at 2
-(add-hook 'hledger-mode-hook '(lambda() (setq tab-width 2)))
