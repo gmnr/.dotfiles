cc0ea51814da4d683e72bdd1612a00a7fb5a2c09
gmnr <guido.minieri@gmail.com>
2023-05-08 12:20:15 +0200

    move tmp buffer function into hledger module

--- emacs/lisp/functions.el
+++ emacs/lisp/functions.el
@@ -1,18 +1,7 @@
-;; hledger mode wrapper
-(defun temp-wrapper (report-name cmd)
-  "Send shell commands to tmp-buffer"
-  (interactive)
-  (with-output-to-temp-buffer (concat"*" report-name "*")
-    (shell-command cmd
-                   (concat"*" report-name "*")
-                   "*Messages*")
-    (pop-to-buffer (concat"*" report-name "*")))
-  (special-mode)
-  (delete-other-windows))
-
-
 (defun kill-all-buffers ()
   "Kill all buffers except current one and toolkit (*Messages*, *scratch*). Close other windows."
   (interactive)
   (mapcar 'kill-buffer (buffer-list))
   (delete-other-windows))
+
+
