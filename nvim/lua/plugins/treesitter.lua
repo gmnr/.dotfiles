26dc74145012efbe0f096d25d1d6fe301ccbac4c
gmnr <guido.minieri@gmail.com>
2023-06-05 22:03:39 +0200

    force .md on treesetter for lspsaga but disable it

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,12 +4,13 @@ function M.config()
   require('nvim-treesitter.configs').setup {
     highlight = {
       enable = true,
+      disable = {'markdown'}
     },
     indent = {
       enabled = false,
       disable = {},
     },
-    ensure_installed = {'bash', 'css', 'html', 'javascript', 'json5', 'ledger', 'lua', 'norg', 'python'},
+    ensure_installed = {'bash', 'css', 'html', 'javascript', 'json5', 'ledger', 'lua', 'markdown', 'markdown_inline', 'norg', 'python'},
     sync_install = true, -- install synchronously
 		ignore_install = {}, -- parsers to not install
   }
