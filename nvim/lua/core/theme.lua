768ba88cd5040dc84db4cc264b64d68a75014736
gmnr <guido.minieri@gmail.com>
2023-06-28 17:58:54 +0200

    gitsigns: update colors in accordance with theme

--- nvim/lua/core/theme.lua
+++ nvim/lua/core/theme.lua
@@ -21,3 +21,11 @@ vim.cmd('highlight! SpellBad              guibg=NONE guifg=#E5C07B')
 vim.cmd('highlight! SpellCap              guibg=NONE guifg=#E5C07B')
 vim.cmd('highlight! SpellLocal            guibg=NONE guifg=#E5C07B')
 vim.cmd('highlight! SpellRare             guibg=NONE guifg=#E5C07B')
+
+-- define custom colors for gitsigns
+vim.cmd('highlight! GitSignsAdd           guibg=NONE guifg=#92BE65')
+vim.cmd('highlight! GitSignsAddNr         guibg=NONE guifg=#92BE65')
+vim.cmd('highlight! GitSignsChange        guibg=NONE guifg=#7AA2F7')
+vim.cmd('highlight! GitSignsChangeNr      guibg=NONE guifg=#7AA2F7')
+vim.cmd('highlight! GitSignsDelete        guibg=NONE guifg=#D74B4B')
+vim.cmd('highlight! GitSignsDeleteNr      guibg=NONE guifg=#D74B4B')
