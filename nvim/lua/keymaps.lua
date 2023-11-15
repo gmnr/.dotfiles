ed3ac2b292c92106963b63eea5ea44b13f29916b
gmnr <guido.minieri@gmail.com>
2023-11-16 00:55:52 +0100

    formatter: add formatter

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,149 +1,157 @@
 -- MAPPINGS
 -- fix Y behaviour
-vim.keymap.set('n', 'Y', 'y$')
+vim.keymap.set("n", "Y", "y$")
 
 -- saner line navigation
-vim.keymap.set('n', 'M', '^')
-vim.keymap.set('n', 'I', '$')
+vim.keymap.set("n", "M", "^")
+vim.keymap.set("n", "I", "$")
 
 -- remap movement for colemak
-vim.keymap.set({'n', 'v'}, 'm', 'h')
-vim.keymap.set({'n', 'v'}, 'n', 'gj')  -- with movement across visual line
-vim.keymap.set({'n', 'v'}, 'e', 'gk')  -- with movement across visual line
-vim.keymap.set({'n', 'v'}, 'i', 'l')
+vim.keymap.set({ "n", "v" }, "m", "h")
+vim.keymap.set({ "n", "v" }, "n", "gj") -- with movement across visual line
+vim.keymap.set({ "n", "v" }, "e", "gk") -- with movement across visual line
+vim.keymap.set({ "n", "v" }, "i", "l")
 
 -- remap old hjkl & capital
-vim.keymap.set({'n', 'v'}, 'h', 'e')
-vim.keymap.set({'n', 'v'}, 'j', 'm')
-vim.keymap.set({'n', 'v'}, 'k', 'n')
-vim.keymap.set({'n', 'v'}, 'l', 'i')
-vim.keymap.set({'n', 'v'}, 'H', 'E')
-vim.keymap.set({'n', 'v'}, 'K', 'N')
-vim.keymap.set({'n', 'v'}, 'L', 'I')
-vim.keymap.set({'n', 'v'}, 'N', 'J')
+vim.keymap.set({ "n", "v" }, "h", "e")
+vim.keymap.set({ "n", "v" }, "j", "m")
+vim.keymap.set({ "n", "v" }, "k", "n")
+vim.keymap.set({ "n", "v" }, "l", "i")
+vim.keymap.set({ "n", "v" }, "H", "E")
+vim.keymap.set({ "n", "v" }, "K", "N")
+vim.keymap.set({ "n", "v" }, "L", "I")
+vim.keymap.set({ "n", "v" }, "N", "J")
 
 -- remap redo command
-vim.keymap.set('n', '<C-p>', '<C-r>')
+vim.keymap.set("n", "<C-p>", "<C-r>")
 
 -- override scrolling
-vim.keymap.set('n', '<C-f>', '<C-u>')
-vim.keymap.set('n', '<C-s>', '<C-d>')
+vim.keymap.set("n", "<C-f>", "<C-u>")
+vim.keymap.set("n", "<C-s>", "<C-d>")
 
 -- open and load .vimrc
-vim.keymap.set('n', '<leader>fd', ':lua require("telescope.builtin").find_files({cwd="~/.dotfiles/nvim/lua/"})<CR>', { silent = true })
-vim.keymap.set('n', '<leader>fw', ':lua Annotate()<CR>:exe "e +$ ~/.notes/notepad/".strftime("%Y%m%d").".md"<CR>', { silent = true })
-vim.keymap.set('n', '<leader>fu', ':Lazy<CR>')
+vim.keymap.set(
+	"n",
+	"<leader>fc",
+	':lua require("telescope.builtin").find_files({cwd="~/.dotfiles/nvim/lua/"})<CR>',
+	{ silent = true }
+)
+vim.keymap.set(
+	"n",
+	"<leader>fn",
+	':lua Annotate()<CR>:exe "e +$ ~/.notes/notepad/".strftime("%Y%m%d").".md"<CR>',
+	{ silent = true }
+)
+vim.keymap.set("n", "<leader>fu", ":Lazy<CR>")
 
 -- open notes
-vim.keymap.set('n', '<leader>pl', ':lua require("telescope.builtin").find_files({cwd="~/.notes"})<CR>', { silent = true })
+vim.keymap.set(
+	"n",
+	"<leader>pl",
+	':lua require("telescope.builtin").find_files({cwd="~/.notes"})<CR>',
+	{ silent = true }
+)
 
 -- quick save and exit
-vim.keymap.set('n', '<leader>ww', ':w<CR>')
-vim.keymap.set('n', '<leader>wq', ':Wd<CR>')
-vim.keymap.set('n', '<leader>qq', ':bd!<CR>')
-vim.keymap.set('n', '<leader>QQ', ':q!<CR>')
-vim.keymap.set('n', '<leader>QA', ':qa!<CR>')
+vim.keymap.set("n", "<leader>ww", ":w<CR>")
+vim.keymap.set("n", "<leader>wq", ":Wd<CR>")
+vim.keymap.set("n", "<leader>qq", ":bd!<CR>")
+vim.keymap.set("n", "<leader>QQ", ":q!<CR>")
+vim.keymap.set("n", "<leader>QA", ":qa!<CR>")
 
 -- open and close quickfix and location list
-vim.keymap.set('n', '<leader>qo', ':cope<CR>')
-vim.keymap.set('n', '<leader>qc', ':ccl<CR>')
-vim.keymap.set('n', '<leader>lo', ':lopen<CR>')
-vim.keymap.set('n', '<leader>lc', ':lclose<CR>')
+vim.keymap.set("n", "<leader>qo", ":cope<CR>")
+vim.keymap.set("n", "<leader>qc", ":ccl<CR>")
+vim.keymap.set("n", "<leader>lo", ":lopen<CR>")
+vim.keymap.set("n", "<leader>lc", ":lclose<CR>")
 
 --  quick search
-vim.keymap.set('n', '<leader>tt', ':%s/')
-
--- start interactive EasyAlign in visual mode (e.g. vipga)
-vim.keymap.set({'n', 'x'}, 'ga', '<Plug>(EasyAlign)')
+vim.keymap.set("n", "<leader>tt", ":%s/")
 
 -- remap backspace to lats used buffer
-vim.keymap.set('n', '<BS>', '<C-^>')
+vim.keymap.set("n", "<BS>", "<C-^>")
 
 -- map vim-fugitive
-vim.keymap.set('n', '<leader>gs', ':Git<CR>')
-vim.keymap.set('n', '<leader>gy', ':Git push<CR>')
-vim.keymap.set('n', '<leader>gm', ':diffget //2<CR>')
-vim.keymap.set('n', '<leader>gi', ':diffget //3<CR>')
+vim.keymap.set("n", "<leader>gs", ":Git<CR>")
+vim.keymap.set("n", "<leader>gy", ":Git push<CR>")
+vim.keymap.set("n", "<leader>gm", ":diffget //2<CR>")
+vim.keymap.set("n", "<leader>gi", ":diffget //3<CR>")
 
 -- telescope mappings
-vim.keymap.set('n', '<leader>yy', ':lua Project_files()<CR>', { silent = true })
-vim.keymap.set('n', '<leader>yi', ':Telescope live_grep<CR>', { silent = true })
-vim.keymap.set('n', '<leader><space>', ':Telescope buffers<CR>', { silent = true })
-vim.keymap.set('n', '<leader>ys', ':Telescope git_status<CR>', { silent = true })
-vim.keymap.set('n', '<leader>yc', ':Telescope git_branches<CR>', { silent = true })
-vim.keymap.set('n', '<leader>ya', ':Telescope find_files<CR>', { silent = true })
-vim.keymap.set('n', '<leader>yh', ':Telescope help_tags<CR>', { silent = true })
-vim.keymap.set('n', '<leader>yd', ':Telescope diagnostics<CR>', { silent = true })
-vim.keymap.set('n', '<leader>yg', ':silent grep ')
-vim.keymap.set('n', '<leader>;;', ':Telescope commands<CR>', { silent = true })
-vim.keymap.set('n', '<leader>yu', ':Telescope ')
+vim.keymap.set("n", "<leader><space>", ":lua Project_files()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>/", ":Telescope live_grep<CR>", { silent = true })
+vim.keymap.set("n", "<leader>;", ":Telescope commands<CR>", { silent = true })
+vim.keymap.set("n", "<leader>,", ":Telescope buffers<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>ys", ":Telescope git_status<CR>", { silent = true })
+vim.keymap.set("n", "<leader>yc", ":Telescope git_branches<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ya", ":Telescope find_files<CR>", { silent = true })
+vim.keymap.set("n", "<leader>yh", ":Telescope help_tags<CR>", { silent = true })
+vim.keymap.set("n", "<leader>yg", ":silent grep ")
+vim.keymap.set("n", "<leader>yu", ":Telescope ")
 
 -- change the current directory to edited file
-vim.keymap.set('n', '<leader>cd', ':cd %:h<CR>')
+vim.keymap.set("n", "<leader>cd", ":cd %:h<CR>")
 
 -- add autocorrect
-vim.keymap.set('n', '<leader><Tab>', '1z=')
+vim.keymap.set("n", "<leader><Tab>", "1z=")
 
 -- quickrun
-vim.keymap.set('n', '<leader>pp', ':w<CR>:QuickRun<CR>')
+vim.keymap.set("n", "<leader>pp", ":w<CR>:QuickRun<CR>")
 
 -- toggle paste and manage copy
-vim.keymap.set('n', '<leader>jj', ':set invpaste paste?<CR>')
-vim.keymap.set('n', '<leader>cp', ':w !pbcopy<CR>')
+vim.keymap.set("n", "<leader>jj", ":set invpaste paste?<CR>")
+vim.keymap.set("n", "<leader>cp", ":w !pbcopy<CR>")
 
 -- change mappings for location list navigation
-vim.keymap.set('n', '[w', ':lprevious<CR>')
-vim.keymap.set('n', ']w', ':lnext<CR>')
-vim.keymap.set('n', '[W', ':lfirst<CR>')
-vim.keymap.set('n', ']W', ':llast<CR>')
+vim.keymap.set("n", "[w", ":lprevious<CR>")
+vim.keymap.set("n", "]w", ":lnext<CR>")
+vim.keymap.set("n", "[W", ":lfirst<CR>")
+vim.keymap.set("n", "]W", ":llast<CR>")
 
 -- change prefix for window navigation
-vim.keymap.set('n', '<leader>a', '<C-w>')
-vim.keymap.set('n', '<leader>am', '<C-w>h')
-vim.keymap.set('n', '<leader>an', '<C-w>j')
-vim.keymap.set('n', '<leader>ae', '<C-w>k')
-vim.keymap.set('n', '<leader>ai', '<C-w>l')
+vim.keymap.set("n", "<leader>aa", "<C-w>w")
+vim.keymap.set("n", "<leader>ao", "<C-w>o")
+vim.keymap.set("n", "<leader>ac", "<C-w>c")
+vim.keymap.set("n", "<leader>a-", "<C-w>v")
+vim.keymap.set("n", "<leader>a=", "<C-w>s")
+vim.keymap.set("n", "<leader>am", "<C-w>h")
+vim.keymap.set("n", "<leader>an", "<C-w>j")
+vim.keymap.set("n", "<leader>ae", "<C-w>k")
+vim.keymap.set("n", "<leader>ai", "<C-w>l")
 
 -- swap fold command
-vim.keymap.set('n', 'za', 'zo')
-vim.keymap.set('n', 'zo', 'za')
+vim.keymap.set("n", "za", "zo")
+vim.keymap.set("n", "zo", "za")
 
 -- hledger formatting
-vim.keymap.set('n', '<leader>bb', ':lua BetterLedgerAlign()<CR>')
-vim.keymap.set('n', '<leader>ba', ':!/usr/local/bin/hledger -f /Users/guido/.finance/all.journal payees > /Users/guido/.finance/.src/completion-source/payees<CR>', { silent = true })
-vim.keymap.set('n', '<leader>bq', ':!/usr/local/bin/hledger -f /Users/guido/.finance/all.journal accounts | /usr/bin/python3 /Users/guido/.finance/.src/scripts/filter-account.py > /Users/guido/.finance/.src/completion-source/accounts<CR>', { silent = true })
+vim.keymap.set("n", "<leader>bb", ":lua BetterLedgerAlign()<CR>")
+vim.keymap.set(
+	"n",
+	"<leader>ba",
+	":!/usr/local/bin/hledger -f /Users/guido/.finance/all.journal payees > /Users/guido/.finance/.src/completion-source/payees<CR>",
+	{ silent = true }
+)
+vim.keymap.set(
+	"n",
+	"<leader>bq",
+	":!/usr/local/bin/hledger -f /Users/guido/.finance/all.journal accounts | /usr/bin/python3 /Users/guido/.finance/.src/scripts/filter-account.py > /Users/guido/.finance/.src/completion-source/accounts<CR>",
+	{ silent = true }
+)
 
 -- gitsigns
-vim.keymap.set('n', '<leader>hp', ':Gitsigns preview_hunk<CR>')
-vim.keymap.set('n', '<leader>hb', ':Gitsigns blame_line<CR>')
-vim.keymap.set('n', '<leader>hB', ':Gitsigns toggle_current_line_blame<CR>')
-vim.keymap.set('n', '<leader>hS', ':Gitsigns stage_buffer<CR>')
-vim.keymap.set('n', '<leader>hR', ':Gitsigns reset_buffer<CR>')
-vim.keymap.set('n', '<leader>hd', ':Gitsigns diffthis<CR>')
-vim.keymap.set('n', '<leader>hu', ':Gitsigns undo_stage_hunk<CR>')
-vim.keymap.set({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', { silent = true })
-vim.keymap.set({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', { silent = true })
-vim.keymap.set('n', ']c', '&diff ? "]c" : ":Gitsigns next_hunk<CR>"', { expr = true })
-vim.keymap.set('n', '[c', '&diff ? "[c" : ":Gitsigns prev_hunk<CR>"', { expr = true })
-
--- lsp config
-vim.keymap.set('n', '<leader>F', ':Lspsaga hover_doc<CR>', { silent = true })
-vim.keymap.set('n', '[d', ':Lspsaga diagnostic_jump_prev<CR>', { silent = true })
-vim.keymap.set('n', ']d', ':Lspsaga diagnostic_jump_next<CR>', { silent = true })
-vim.keymap.set('n', 'gI', vim.lsp.buf.implementation)
-vim.keymap.set('n', '<C-r>', vim.lsp.buf.signature_help)
-vim.keymap.set('n', '<leader>ls', vim.diagnostic.setloclist)
-vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition)
-vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
-vim.keymap.set('n', 'gd', ':Telescope lsp_definitions<CR>', {silent = true})
-vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', {silent = true})
-vim.keymap.set('n', '<leader>ll', ':Lspsaga show_line_diagnostics<CR>', { silent = true })
-vim.keymap.set('n', '<leader>lL', ':Lspsaga show_cursor_diagnostics<CR>', { silent = true })
-vim.keymap.set('n', '<leader>la', ':Lspsaga code_action<CR>', { silent = true })
-vim.keymap.set('n', '<leader>lf', ':Lspsaga lsp_finder<CR>', { silent = true })
-vim.keymap.set('n', '<leader>lr', ':Lspsaga rename<CR>', { silent = true })
+vim.keymap.set("n", "<leader>hp", ":Gitsigns preview_hunk<CR>")
+vim.keymap.set("n", "<leader>hb", ":Gitsigns blame_line<CR>")
+vim.keymap.set("n", "<leader>hB", ":Gitsigns toggle_current_line_blame<CR>")
+vim.keymap.set("n", "<leader>hS", ":Gitsigns stage_buffer<CR>")
+vim.keymap.set("n", "<leader>hR", ":Gitsigns reset_buffer<CR>")
+vim.keymap.set("n", "<leader>hd", ":Gitsigns diffthis<CR>")
+vim.keymap.set("n", "<leader>hu", ":Gitsigns undo_stage_hunk<CR>")
+vim.keymap.set({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>", { silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", { silent = true })
+vim.keymap.set("n", "]c", '&diff ? "]c" : ":Gitsigns next_hunk<CR>"', { expr = true })
+vim.keymap.set("n", "[c", '&diff ? "[c" : ":Gitsigns prev_hunk<CR>"', { expr = true })
 
 --  silence grep when grepping
 vim.cmd("cnoreabbrev <expr> grep  (getcmdtype() ==# ':' && getcmdline() =~# '^grep')  ? 'silent grep'  : 'grep'")
 vim.cmd("cnoreabbrev <expr> lgrep (getcmdtype() ==# ':' && getcmdline() =~# '^lgrep') ? 'silent lgrep' : 'lgrep'")
-
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,172 +1,178 @@
 -- OPTIONS
 -- basic options
-vim.opt.number                     = true  -- enable numbering
-vim.opt.termguicolors              = true  -- enable termguicolors
-vim.opt.shiftround                 = true  -- round indent to multiples of shiftwidth
-vim.opt.cursorline                 = true  -- enable cursorline
-vim.opt.ignorecase                 = true  -- ignore case
-vim.opt.smartcase                  = true  -- override ignorecase if capital in search pattern
-vim.opt.expandtab                  = true  -- use spaces while tabbing
-vim.opt.lazyredraw                 = true  -- don't redraw screen while executing macros
-vim.opt.gdefault                   = true  -- when substituting the g flag is always enabled
-vim.opt.smartindent                = true  -- smart autoindenting
-vim.opt.splitright                 = true  -- open next window to the right of current one
-vim.opt.showmode                   = false -- hide mode on last row
-vim.opt.swapfile                   = false -- disable swapfile
-vim.opt.signcolumn                 = 'yes' -- always enable signcolumn
-vim.opt.tabstop                    = 2     -- number of spaces a tab counts for
-vim.opt.shiftwidth                 = 2     -- number of spaces of each indentation level
-vim.opt.softtabstop                = 2     -- insert spaces when tabbing
-vim.opt.scrolloff                  = 999   -- keep cursor in the middle of the line
-vim.opt.wildignorecase             = true  -- ignore case when writing commands
-vim.opt.complete                   = ''    -- disable ordinary autocompletion
-vim.opt.conceallevel               = 2     -- enable concealing
+vim.opt.number = true -- enable numbering
+vim.opt.termguicolors = true -- enable termguicolors
+vim.opt.shiftround = true -- round indent to multiples of shiftwidth
+vim.opt.cursorline = true -- enable cursorline
+vim.opt.ignorecase = true -- ignore case
+vim.opt.smartcase = true -- override ignorecase if capital in search pattern
+vim.opt.expandtab = true -- use spaces while tabbing
+vim.opt.lazyredraw = true -- don't redraw screen while executing macros
+vim.opt.gdefault = true -- when substituting the g flag is always enabled
+vim.opt.smartindent = true -- smart autoindenting
+vim.opt.splitright = true -- open next window to the right of current one
+vim.opt.showmode = false -- hide mode on last row
+vim.opt.swapfile = false -- disable swapfile
+vim.opt.signcolumn = "yes" -- always enable signcolumn
+vim.opt.tabstop = 2 -- number of spaces a tab counts for
+vim.opt.shiftwidth = 2 -- number of spaces of each indentation level
+vim.opt.softtabstop = 2 -- insert spaces when tabbing
+vim.opt.scrolloff = 999 -- keep cursor in the middle of the line
+vim.opt.wildignorecase = true -- ignore case when writing commands
+vim.opt.complete = "" -- disable ordinary autocompletion
+vim.opt.conceallevel = 2 -- enable concealing
 
 -- set leader
-vim.keymap.set('n', '<Space>', '<Nop>', { silent = true, remap = false })  -- improve performance of leader key
-vim.g.mapleader = ' '
+vim.keymap.set("n", "<Space>", "<Nop>", { silent = true, remap = false }) -- improve performance of leader key
+vim.g.mapleader = " "
 
 -- diff options
-vim.opt.diffopt                    = 'filler,iwhite,internal,algorithm:patience'
+vim.opt.diffopt = "filler,iwhite,internal,algorithm:patience"
 
 -- spelling
-vim.opt.spelllang                  = 'en_us,it'  -- set language of spell
-vim.opt.spellcapcheck              = ''          -- don't check cap error at beginning of line
+vim.opt.spelllang = "en_us,it" -- set language of spell
+vim.opt.spellcapcheck = "" -- don't check cap error at beginning of line
 
 -- grep settings (use ripgrep)
-vim.opt.grepprg                    = 'rg --vimgrep --no-heading --smart-case'
-vim.opt.grepformat                 = '%f:%l:%c:%m,%f:%l:%m'
-
+vim.opt.grepprg = "rg --vimgrep --no-heading --smart-case"
+vim.opt.grepformat = "%f:%l:%c:%m"
 
 -- AUTOCOMMANDS
 local api = vim.api
 
-api.nvim_create_autocmd('BufNewFile', {
-  desc = 'Import template for python files',
-  pattern = {'*.py'},
-  command = '0r ~/.dotfiles/nvim/templates/python.template'
+api.nvim_create_autocmd("BufNewFile", {
+	desc = "Import template for python files",
+	pattern = { "*.py" },
+	command = "0r ~/.dotfiles/nvim/templates/python.template",
 })
 
-api.nvim_create_autocmd('FileType', {
-  desc = 'Verbose texts should have a default textwidth and spelling enabled',
-  pattern = {'markdown', 'text'},
-  command = 'setlocal spell tw=80'
+api.nvim_create_autocmd("FileType", {
+	desc = "Verbose texts should have a default textwidth and spelling enabled",
+	pattern = { "markdown", "text" },
+	command = "setlocal spell tw=80",
 })
 
-api.nvim_create_autocmd('FileType', {
-  desc = 'Disable spell in help files',
-  pattern = {'help'},
-  command = 'setlocal nospell'
+api.nvim_create_autocmd("FileType", {
+	desc = "Disable spell in help files",
+	pattern = { "help" },
+	command = "setlocal nospell",
 })
 
-api.nvim_create_autocmd('FileType', {
-  desc = 'Indent at 2 spaces on ledger files',
-  pattern = {'ledger'},
-  command = 'setlocal ts=2 sw=2 sts=2'
+api.nvim_create_autocmd("FileType", {
+	desc = "Indent at 2 spaces on ledger files",
+	pattern = { "ledger" },
+	command = "setlocal ts=2 sw=2 sts=2",
 })
 
-api.nvim_create_autocmd('FileType', {
-  desc = 'Autoalign amounts on ledger files when pressing enter',
-  pattern = {'ledger'},
-  command = 'inoremap <silent> <CR> <C-r>=v:lua.BetterLedgerAlign()<CR><Right><CR>'
+api.nvim_create_autocmd("FileType", {
+	desc = "Autoalign amounts on ledger files when pressing enter",
+	pattern = { "ledger" },
+	command = "inoremap <silent> <CR> <C-r>=v:lua.BetterLedgerAlign()<CR><Right><CR>",
 })
 
-api.nvim_create_autocmd('FileType', {
-  desc = 'automatically close the quickfixlist with q',
-  pattern = {'qf'},
-  command = 'nmap <buffer> q :ccl<CR>'
+api.nvim_create_autocmd("FileType", {
+	desc = "automatically close the quickfixlist with q",
+	pattern = { "qf" },
+	command = "nmap <buffer> q :ccl<CR>",
 })
 
-api.nvim_create_autocmd('FileType', {
-  desc = 'Start git messages in insert mode',
-  pattern  = { 'gitcommit', 'gitrebase', },
-  command  = 'startinsert | 1'
+api.nvim_create_autocmd("FileType", {
+	desc = "Start git messages in insert mode",
+	pattern = { "gitcommit", "gitrebase" },
+	command = "startinsert | 1",
 })
 
-api.nvim_create_autocmd('BufWritePre', {
-  desc = 'Remove trailing spaces',
-  pattern = {'*.journal'},
-  command = '%s/\\s\\+$//e'
+api.nvim_create_autocmd("BufWritePre", {
+	desc = "Remove trailing spaces",
+	pattern = { "*.journal" },
+	command = "%s/\\s\\+$//e",
 })
 
-api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight yanked text',
-  group = api.nvim_create_augroup('yank_highlight', {}),
-  pattern = '*',
-  callback = function()
-    vim.highlight.on_yank{hlgroup='IncSearch', timeout=300}
-  end,
+api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight yanked text",
+	group = api.nvim_create_augroup("yank_highlight", {}),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ hlgroup = "IncSearch", timeout = 300 })
+	end,
 })
 
-local OpenQuickfix = api.nvim_create_augroup('OpenQuickfix', { clear = true })
-api.nvim_create_autocmd('QuickFixCmdPost', {
-  desc = 'Open quickfix list when running a quickfix command',
-  group = OpenQuickfix,
-  pattern = '[^l]*',
-  command = 'cwindow'
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
 
-api.nvim_create_autocmd('QuickFixCmdPost', {
-  desc = 'Open location list when running a location command',
-  group = OpenQuickfix,
-  pattern = 'l*',
-  command = 'lwindow'
+local OpenQuickfix = api.nvim_create_augroup("OpenQuickfix", { clear = true })
+api.nvim_create_autocmd("QuickFixCmdPost", {
+	desc = "Open quickfix list when running a quickfix command",
+	group = OpenQuickfix,
+	pattern = "[^l]*",
+	command = "cwindow",
 })
 
+api.nvim_create_autocmd("QuickFixCmdPost", {
+	desc = "Open location list when running a location command",
+	group = OpenQuickfix,
+	pattern = "l*",
+	command = "lwindow",
+})
 
 -- FUNCTIONS
 -- save and close buffer
-vim.api.nvim_create_user_command('Wd', 'write|bdelete', {})
+vim.api.nvim_create_user_command("Wd", "write|bdelete", {})
 
 -- Align ledger amounts and add trailing zeros when needed
 function _G.BetterLedgerAlign()
-  if string.find(vim.fn.getline('.'):sub(-1), '%d') then
-    local str = vim.fn.getline('.')
-    local elements = {}
+	if string.find(vim.fn.getline("."):sub(-1), "%d") then
+		local str = vim.fn.getline(".")
+		local elements = {}
 
-    for el in str:gmatch('%S+') do table.insert(elements, el) end
+		for el in str:gmatch("%S+") do
+			table.insert(elements, el)
+		end
 
-    local amt = elements[#elements]
-    if not string.find(amt, ',') then
-      vim.cmd('normal A,00')
-    else
-      local pos = string.find(amt, ',')
-      if pos == #amt-1 then
-        vim.cmd('normal A0')
-      end
-    end
-    vim.cmd(':LedgerAlign')
-    return ''
-  else
-    return ''
-  end
+		local amt = elements[#elements]
+		if not string.find(amt, ",") then
+			vim.cmd("normal A,00")
+		else
+			local pos = string.find(amt, ",")
+			if pos == #amt - 1 then
+				vim.cmd("normal A0")
+			end
+		end
+		vim.cmd(":LedgerAlign")
+		return ""
+	else
+		return ""
+	end
 end
 
 -- write a note in the editor
 function _G.Annotate()
+	local date = os.date("%Y%m%d")
+	local time = os.date("%H:%M")
 
-  local date = os.date('%Y%m%d')
-  local time = os.date('%H:%M')
+	local filename = date .. ".md"
+	local fullpath = "/Users/guido/.notes/notepad/" .. filename
 
-  local filename = date .. '.md'
-  local fullpath = '/Users/guido/.notes/notepad/' .. filename
-
-  local f = io.open(fullpath, 'r')
-  if f ~= nil and io.close(f) then
-    local file = io.open(fullpath, 'a')
-    file:write('\n> UPDATED @ ' .. time)
-    file:close()
-  else
-    local file = io.open(fullpath, 'w')
-    file:write('# ' .. os.date('%Y-%m-%d'))
-    file:close()
-  end
+	local f = io.open(fullpath, "r")
+	if f ~= nil and io.close(f) then
+		local file = io.open(fullpath, "a")
+		file:write("\n> UPDATED @ " .. time)
+		file:close()
+	else
+		local file = io.open(fullpath, "w")
+		file:write("# " .. os.date("%Y-%m-%d"))
+		file:close()
+	end
 end
 
 -- go to root dir when using telescope
 function _G.Project_files()
-  local ok = pcall(require('telescope.builtin').git_files)
-  if not ok then
-    require('telescope.builtin').find_files()
-  end
+	local ok = pcall(require("telescope.builtin").git_files)
+	if not ok then
+		require("telescope.builtin").find_files()
+	end
 end
