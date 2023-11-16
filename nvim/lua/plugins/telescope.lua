60e1dcaa574081c52d86c83c4af1f43bdab630a4
gmnr <guido.minieri@gmail.com>
2023-11-16 17:16:06 +0100

    telescope: fix overwriting of config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,52 +13,50 @@ return {
         end,
       },
     },
-    opts = {
-      defaults = {
-        layout_strategy = "horizontal",
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.5,
-          },
-          width = 0.8,
-          height = 0.8,
-          preview_cutoff = 120,
-        },
-        sorting_strategy = "ascending",
-        winblend = 0,
-      },
-    },
-    config = function()
-      require("telescope").setup({
+    opts = function()
+      local actions = require("telescope.actions")
+
+      return {
         defaults = {
           mappings = {
             i = {
-              ["<ESC>"] = require("telescope.actions").close, -- don't enable normal mode
+              ["<ESC>"] = actions.close, -- don't enable normal mode
               ["<C-u>"] = false, -- clear prompt with C-u
-              ["<C-n>"] = require("telescope.actions").move_selection_next,
-              ["<C-e>"] = require("telescope.actions").move_selection_previous,
-              ["<C-k>"] = require("telescope.actions").preview_scrolling_down,
-              ["<C-y>"] = require("telescope.actions").preview_scrolling_up,
+              ["<C-n>"] = actions.move_selection_next,
+              ["<C-e>"] = actions.move_selection_previous,
+              ["<C-k>"] = actions.preview_scrolling_down,
+              ["<C-y>"] = actions.preview_scrolling_up,
+            },
+          },
+          prompt_prefix = " ",
+          selection_caret = " ",
+          layout_strategy = "horizontal",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.5,
+            },
+            width = 0.8,
+            height = 0.8,
+            preview_cutoff = 120,
+          },
+          sorting_strategy = "ascending",
+          winblend = 0,
+          pickers = {
+            find_files = {
+              find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" }, -- look into hidden dirs but not .git
+            },
+          },
+          extensions = {
+            fzf = {
+              fuzzy = true,
+              override_generic_sorter = true,
+              override_file_sorter = true,
+              case_mode = "smart_case",
             },
           },
         },
-        pickers = {
-          find_files = {
-            find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" }, -- look into hidden dirs but not .git
-          },
-        },
-        extensions = {
-          fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = "smart_case",
-          },
-        },
-      })
-
-      pcall(require("telescope").load_extension, "fzf")
+      }
     end,
   },
 }
