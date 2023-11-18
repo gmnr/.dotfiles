9d4e88893d76a31b6f6bc99f0818f5b77785dfd9
gmnr <guido.minieri@gmail.com>
2023-11-18 20:54:57 +0100

    nvim: add ledger custom function to config

--- nvim/lua/config/functions.lua
+++ nvim/lua/config/functions.lua
@@ -1,32 +1,6 @@
 -- FUNCTIONS
 -- save and close buffer
-vim.api.nvim_create_user_command("Wd", "write|bdelete", {})
-
--- Align ledger amounts and add trailing zeros when needed
-function _G.better_ledger_align()
-  if string.find(vim.fn.getline("."):sub(-1), "%d") then
-    local str = vim.fn.getline(".")
-    local elements = {}
-
-    for el in str:gmatch("%S+") do
-      table.insert(elements, el)
-    end
-
-    local amt = elements[#elements]
-    if not string.find(amt, ",") then
-      vim.cmd("normal A,00")
-    else
-      local pos = string.find(amt, ",")
-      if pos == #amt - 1 then
-        vim.cmd("normal A0")
-      end
-    end
-    vim.cmd(":LedgerAlign")
-    return ""
-  else
-    return ""
-  end
-end
+vim.api.nvim_create_user_command("WRCL", "write|bdelete", {})
 
 -- write a note in the editor
 function _G.annotate()
--- nvim/lua/plugins/filetypes.lua
+++ nvim/lua/plugins/filetypes.lua
@@ -16,6 +16,32 @@ return {
     config = function()
       vim.g.ledger_bin = "hledger"
       vim.g.ledger_decimal_sep = ","
+
+      -- define function to align at <CR>
+      function better_ledger_align()
+        if string.find(vim.fn.getline("."):sub(-1), "%d") then
+          local str = vim.fn.getline(".")
+          local elements = {}
+
+          for el in str:gmatch("%S+") do
+            table.insert(elements, el)
+          end
+
+          local amt = elements[#elements]
+          if not string.find(amt, ",") then
+            vim.cmd("normal A,00")
+          else
+            local pos = string.find(amt, ",")
+            if pos == #amt - 1 then
+              vim.cmd("normal A0")
+            end
+          end
+          vim.cmd(":LedgerAlign")
+          return ""
+        else
+          return ""
+        end
+      end
     end,
     ft = { "ledger" },
     keys = {
