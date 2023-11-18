5c3b37691b36dc14f195b37926d05458d2aa0cb8
gmnr <guido.minieri@gmail.com>
2023-11-18 21:06:30 +0100

    nvim: solve hledger bug

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -16,7 +16,7 @@ return {
 
   {
     "windwp/nvim-autopairs",
-    event = "VeryLazy",
+    -- event = "VeryLazy",
     config = function()
       local npairs = require("nvim-autopairs")
       local Rule = require("nvim-autopairs.rule")
