adfb8f4a46e67ce520d75b223b3d8dd0aef13832
gmnr <guido.minieri@gmail.com>
2023-11-19 19:43:49 +0100

    nvim: add vscode symbols to autocompletion

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -9,6 +9,7 @@ return {
       "hrsh7th/cmp-cmdline",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-path",
+      "onsails/lspkind.nvim",
       {
         "SirVer/ultisnips",
         config = function()
@@ -24,6 +25,16 @@ return {
     config = function()
       -- Completion config
       local cmp = require("cmp")
+      local lspkind = require("lspkind")
+      cmp.setup({
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text", -- show only symbol annotations
+            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          }),
+        },
+      })
 
       -- disable auto completion for text heavy types
       cmp.setup.filetype({
--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -10,20 +10,6 @@ return {
     config = function()
       vim.cmd("colorscheme tokyonight-storm")
 
-      -- add colors to types in autocompletion
-      -- vim.cmd("highlight! CmpItemAbbrDeprecated guibg=NONE guifg=#808080")
-      -- vim.cmd("highlight! CmpItemAbbrMatch      guibg=NONE guifg=#98C379")
-      -- vim.cmd("highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#98C379")
-      -- vim.cmd("highlight! CmpItemKindInterface  guibg=NONE guifg=#98C379")
-      -- vim.cmd("highlight! CmpItemKindUnit       guibg=NONE guifg=#98C379")
-      -- vim.cmd("highlight! CmpItemKindFunction   guibg=NONE guifg=#C678DD")
-      -- vim.cmd("highlight! CmpItemKindKeyword    guibg=NONE guifg=#C678DD")
-      -- vim.cmd("highlight! CmpItemKindMethod     guibg=NONE guifg=#E06C75")
-      -- vim.cmd("highlight! CmpItemKindProperty   guibg=NONE guifg=#E06C75")
-      -- vim.cmd("highlight! CmpItemKindVariable   guibg=NONE guifg=#61AFEF")
-      -- vim.cmd("highlight! CmpItemKindText       guibg=NONE guifg=#ABB2BF")
-      -- vim.cmd("highlight! CmpItemKindClass      guibg=NONE guifg=#E5C07B")
-
       -- -- modify spell error highlighings
       vim.cmd("highlight! SpellBad              guibg=NONE guifg=#E5C07B")
       vim.cmd("highlight! SpellCap              guibg=NONE guifg=#E5C07B")
