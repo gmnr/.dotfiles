340b7fae8dac8ab8d96a75bf86b876a88e390ee9
gmnr <guido.minieri@gmail.com>
2023-11-20 21:31:43 +0100

    keys: save and quit cmd

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -54,6 +54,7 @@ vim.keymap.set("n", "<leader>ww", "<cmd>w<CR>", opts)
 vim.keymap.set("n", "<leader>wq", "<cmd>WRCL<CR>", opts)
 vim.keymap.set("n", "<leader>qq", "<cmd>bd!<CR>", opts)
 vim.keymap.set("n", "<leader>QQ", "<cmd>q!<CR>", opts)
+vim.keymap.set("n", "<leader>WQ", "<cmd>wq!<CR>", opts)
 vim.keymap.set("n", "<leader>QA", "<cmd>qa!<CR>", opts)
 
 -- open and close quickfix and location list
