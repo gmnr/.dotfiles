ae0e837e1b2fef753310312169c64686005e9eea
gmnr <guido.minieri@gmail.com>
2023-11-24 10:38:47 +0100

    nvim: remove noice and use plaintext cmdline

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -163,34 +163,4 @@ return {
       extensions = { "fugitive" },
     },
   },
-
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        lsp_doc_border = true,
-      },
-      routes = {
-        {
-          view = "notify",
-          filter = { event = "msg_showmode" },
-        },
-      },
-    },
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-  },
 }
