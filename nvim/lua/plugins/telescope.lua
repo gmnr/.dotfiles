7388804019848b794054378603870ac2de9bff06
gmnr <guido.minieri@gmail.com>
2024-09-02 12:35:54 +0200

    telescope: enable fzf extension

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -19,6 +19,9 @@ return {
         cond = function()
           return vim.fn.executable("make") == 1
         end,
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
       },
       {
         "SirVer/ultisnips",
