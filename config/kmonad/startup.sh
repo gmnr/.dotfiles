557b5d74a4c4268f3bdedc4a3c0fa0fcdf0747dc
gmnr <guido.minieri@gmail.com>
2024-09-16 13:29:41 +0200

    keys: add new keyboard to startup script

--- config/kmonad/startup.sh
+++ config/kmonad/startup.sh
@@ -1,3 +1,10 @@
-# run kmonad for both keyboards
-sudo kmonad ~/.dotfiles/config/kmonad/apple.kbd &
-sudo kmonad ~/.dotfiles/config/kmonad/kinesis.kbd &
+# run kmonad for both "main" keyboards
+if [[ $1 -eq 1 ]]; then
+  sudo kmonad ~/.dotfiles/config/kmonad/apple.kbd &
+  sudo kmonad ~/.dotfiles/config/kmonad/kinesis.kbd &
+fi
+
+# run kmonad for additional keyboard
+if [[ $1 -eq 2 ]]; then
+  sudo kmonad ~/.dotfiles/config/kmonad/fujitsu.kbd &
+fi
