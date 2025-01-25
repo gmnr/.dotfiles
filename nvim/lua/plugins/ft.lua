54f8414f7d7879ed6dd43c366c9e5931b8e9fdc7
gmnr <guido.minieri@gmail.com>
2025-01-25 15:35:17 +0100

    markview: update based on breaking changes

--- nvim/lua/plugins/ft.lua
+++ nvim/lua/plugins/ft.lua
@@ -54,13 +54,15 @@ return {
     ft = "markdown",
     config = function()
       require("markview").setup({
-        modes = { "n", "i", "no", "c" },
-        hybrid_modes = { "i" },
-        callbacks = {
-          on_enable = function(_, win)
-            vim.wo[win].conceallevel = 2
-            vim.wo[win].concealcursor = "nc"
-          end,
+        preview = {
+          modes = { "n", "i", "no", "c" },
+          hybrid_modes = { "i" },
+          callbacks = {
+            on_enable = function(_, win)
+              vim.wo[win].conceallevel = 2
+              vim.wo[win].concealcursor = "nc"
+            end,
+          },
         },
       })
     end,
