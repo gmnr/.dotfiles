605dea42a558302a47f39961f61662663ab299b0
gmnr <guido.minieri@gmail.com>
2025-08-10 09:16:22 +0200

    scripts: add formatting scrpit

---
+++ scripts/sql-formatter.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Wrap prettier-sql-plugin into a python script to format sql scripts
+"""
+
+__author__ = "Guido Minieri"
+__license__ = "GPL"
+
+
+import sys
+import os
+from subprocess import Popen, PIPE
+
+# read input
+sql = sys.stdin.read().rstrip()
+
+# create tmp file to be formatted and read
+with open("/tmp/formatting-tmp.sql", "w") as f:
+    f.write(sql)
+
+my_env = {
+    **os.environ,
+    "PATH": "/opt/homebrew/bin:" + os.environ["PATH"],
+}
+
+# execute command
+p = Popen(
+    env=my_env,
+    cwd="/Users/gmnr/.local/share/nvim/mason/packages/prettier",
+    args=[
+        "/Users/gmnr/.local/share/nvim/mason/bin/prettier",
+        "/tmp/formatting-tmp.sql",
+        "--config",
+        "/Users/gmnr/.local/share/nvim/mason/packages/prettier/.prettierrc.yml",
+    ],
+    stdout=PIPE,
+    stderr=PIPE,
+)
+stdout, stderr = p.communicate()
+sql = stdout.decode("ascii")
+
+# transform and fix small errors
+add_one = False
+if "RIGHT" in sql or "INNER" in sql:
+    add_one = True
+lines = sql.splitlines()
+sql = []
+for line in lines:
+    if " JOIN " in line and " ON " in line:
+        if "RIGHT" in line or "INNER" in line:
+            line = line.lstrip()
+        offset = line.find(" JOIN ") + 3
+        pos = line.find(" ON ")
+        new_pos = line[::-1].find(" NO ") * -1
+        sql.append(line[:pos])
+        sql.append(" " * (offset) + "ON " + line[new_pos:])
+        continue
+
+    if " WHEN " in line or " ELSE " in line:
+        sql.append(line[6:])
+        continue
+
+    if add_one:
+        sql.append(" " + line)
+    else:
+        sql.append(line)
+
+sql = "\n".join(sql)
+print(sql, end="")
