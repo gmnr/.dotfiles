28e40588f62c9425e19b3d4ef1a5ccdc9c8241d2
gmnr <guido.minieri@gmail.com>
2026-04-08 15:24:07 +0200

    remove picker notifier

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -187,7 +187,6 @@ return {
           },
         },
       },
-      notifier = { enabled = true },
       quickfile = { enabled = true },
       scope = { enabled = true },
       scroll = { enabled = true },
