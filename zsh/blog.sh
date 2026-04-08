34256307b86c2fe11723843253a4cea19f72f1d6
gmnr <guido.minieri@gmail.com>
2026-04-08 17:54:50 +0200

    tmux: add script for starting up blogging env

--- zsh/blog.sh
+++ zsh/blog.sh
@@ -10,22 +10,19 @@ then
     # Start New Session with our name
     tmux new-session -d -s $SESSION
 
-    # Name first Pane and start zsh
-    tmux rename-window -t $SESSION:1 'code'
-    tmux send-keys -t $SESSION:1 'zsh' C-m 'clear' C-m
-    tmux send-keys -t $SESSION:1 'cd ~/.blog' C-M 'clear' C-m
+    # start server
+    tmux rename-window -t $SESSION 'server'
+    tmux send-keys -t $SESSION 'cd ~/.blog' C-M 'clear' C-m
+    tmux send-keys -t $SESSION 'hugo server -D -F' C-m
+    tmux split-window -t $SESSION
+    tmux send-keys -t $SESSION 'cd ~/.blog' C-M 'clear' C-m
 
-    # setup Writing window
-    tmux new-window -t $SESSION:2 -n 'writing'
-    tmux send-keys -t $SESSION:2 'cd ~/.blog' C-M 'clear' C-m
-    tmux send-keys -t $SESSION:2 "nvim" C-m
-
-    # Create and setup pane for hugo server
-    tmux new-window -t $SESSION:3 -n 'server'
-    tmux send-keys -t $SESSION:3 'cd ~/.blog' C-M 'clear' C-m
-    tmux send-keys -t $SESSION:3 'hugo server -D -F' C-m
+    # setup editing window
+    tmux new-window -t $SESSION -n 'coding'
+    tmux send-keys -t $SESSION 'cd ~/.blog' C-M 'clear' C-m
+    tmux send-keys -t $SESSION "nvim" C-m
 
 fi
 
 # attach session
-tmux attach-session -d -t $SESSION
+tmux switch-client -t $SESSION
