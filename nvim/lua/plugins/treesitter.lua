54acfba5af810ac654cfbdb3e0ffcd9e0ea12d51
gmnr <guido.minieri@gmail.com>
2026-04-12 20:18:21 +0200

    treesitter: disable indent

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,15 +3,15 @@ return {
   -- treesitter
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = 'main',
+    branch = "main",
     build = ":TSUpdate",
     init = function()
-      vim.api.nvim_create_autocmd('FileType', {
-        callback = function()
-          pcall(vim.treesitter.start)
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
+      -- vim.api.nvim_create_autocmd('FileType', {
+      --   callback = function()
+      --     pcall(vim.treesitter.start)
+      --     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      --   end,
+      -- })
 
       local ensureInstalled = {
         "bash",
@@ -27,13 +27,14 @@ return {
         "python",
         "sql",
       }
-      local alreadyInstalled = require('nvim-treesitter.config').get_installed()
-      local parsersToInstall = vim.iter(ensureInstalled)
-          :filter(function(parser)
-            return not vim.tbl_contains(alreadyInstalled, parser)
-          end)
-          :totable()
-      require('nvim-treesitter').install(parsersToInstall)
-    end
-  }
+      local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+      local parsersToInstall = vim
+        .iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+      require("nvim-treesitter").install(parsersToInstall)
+    end,
+  },
 }
