3a4590e6174dbbd9885757a19c08d1dd4e718e8d
gmnr <guido.minieri@gmail.com>
2026-04-20 16:53:39 +0200

    neogit: disable description on keymap

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -10,7 +10,7 @@ return {
     },
     cmd = "Neogit",
     keys = {
-      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
+      { "<leader>gg", "<cmd>Neogit<cr>" },
     },
     opts = {
       kind = "vsplit",
