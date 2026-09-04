b535998e520fc8025d1f978cf6895649d77793da
gmnr <guido.minieri@gmail.com>
2026-09-04 10:37:53 +0200

    tmux: add keybindings to create/switch sessions

--- tmux/tmux-scripts/advent.sh
+++ tmux/tmux-scripts/advent.sh
@@ -3,7 +3,7 @@
 # Set Session Name
 SESSION="advent"
 SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
-ROOT="${HOME}/Documents/advent-of-code/"
+ROOT=$HOME/Documents/advent-of-code/
 
 YEAR=$(date +"%Y")
 MONTH=$(date +"%m")
@@ -16,17 +16,17 @@ then
     if [[ $MONTH -eq  12 ]] && [[ $DAY -lt 13 ]]
     then
         tmux new-session -d -s $SESSION "cd $ROOT/utils; python3 get_input.py; cd $ROOT/utils; cat $ROOT/$YEAR/$DAY/input.txt"
-        tmux rename-window -t $SESSION 'advent'
         tmux split-window -t $SESSION -c "#{pane_current_path}"
-        tmux new-window -t $SESSION -n 'coding' "nvim $ROOT/$YEAR/$DAY.py"
+        tmux rename-window -t $SESSION "advent"
+        tmux new-window -t $SESSION -n "coding" "nvim $ROOT/$YEAR/$DAY.py; zsh"
 
     else
-        tmux new-session -d -s $SESSION "cd $ROOT; git status"
-        tmux split-window -t $SESSION:1 -c "#{pane_current_path}"
-        tmux rename-window -t $SESSION 'advent'
-        tmux new-window -t $SESSION -n "coding" "cd $ROOT; nvim"
+        tmux new-session -d -s $SESSION "cd $ROOT; git status; zsh"
+        tmux rename-window -t $SESSION "folder"
+        tmux new-window -t $SESSION -n "coding" "cd $ROOT; nvim; zsh"
     fi
 fi
 
 # attach session
 tmux switch-client -t $SESSION
+tmux refresh-client -S
--- tmux/tmux-scripts/blog.sh
+++ tmux/tmux-scripts/blog.sh
@@ -10,11 +10,12 @@ if [[ "$SESSIONEXISTS" = "" ]]
 then
     tmux new-session -d -s $SESSION "cd $ROOT; hugo server -D -F"
     tmux split-window -t $SESSION:1 -c "#{pane_current_path}"
-    tmux rename-window -t $SESSION 'server'
+    tmux rename-window -t $SESSION "server"
 
     # setup editing window
-    tmux new-window -t $SESSION -n "writing" "cd $ROOT; nvim"
+    tmux new-window -t $SESSION -n "writing" "cd $ROOT; nvim; zsh"
 fi
 
 # attach session
 tmux switch-client -t $SESSION
+tmux refresh-client -S
